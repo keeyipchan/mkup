006161e5a05a95b56c8176e7e9f3e4a1eceea36d
keeyipchan <keeyip@huddler-inc.com>
2012-11-14 08:42:02 -0800

    Refactored to work for generating DOM and Objects, mkup.dom and mkup.obj

--- README.md
+++ README.md
@@ -5,18 +5,29 @@ Generate DOM elements from simple one-liners.
 * jQuery
 
 ## How to use
-function mkup(oneliner, onFinish)
+mkup.dom = function(oneliner, onFinish) returns $fragment
+mkup.obj = function(onliner, onFinish) returns Object
 
+### 
 ```js
-mkup('article(form#blah.section.with-tools(a.edit a.clone a.purge(span.your-name)) .section.with-txt(textarea) .section.with-actions())',
-	function($article)
-	{
-		$article.appendTo($('body'))
-	}
-)
+mkup.dom('article(form#blah.section.with-tools(a.edit a.clone a.purge(span.your-name)) .section.with-txt(textarea) .section.with-actions())',
+	function($fragment) {
+		$fragment.appendTo($('body'))
+	})
+	
+mkup.obj('what(more less) article(form#blah.section.with-tools(a.edit a.clone a.purge(span.your-name)) .section.with-txt(textarea) .section.with-actions())',
+	function(obj) {
+		console.dir(obj)
+		console.log(JSON.stringify(obj))
+	})
 ```
 
-Selector syntax: [tag] [id] [classes]
+mkup.dom's selector syntax: [tag] [id] [classes]
 ```js
 /^(.*?)(#.*?)?(\..*?)?$/
+```
+
+mkup.obj's selector syntax: anything except whitespace
+```js
+\S
 ```
--- mkup.js
+++ mkup.js
@@ -1,7 +1,83 @@
-mkup = (function() {
+var mkup = (function() {
 	var kSeparator = /[() ]/
-	var kSelector = /^([a-z].*?)?(#.*?)?(\..*?)?$/
 	
+	// Number of iterations per step before calling next(), which continues in the next tick
+	var kStepSize = 20
+	
+	function readSelector (str, i)
+	{
+		var j=i, n=str.length
+		for (; j < n && !kSeparator.test(str[j]); j++);
+		return str.substring(i,j)
+	}
+
+	return function(str, config)
+	{
+		var path = [config.makeRoot()]
+		var i = 0, n = str.length
+
+		function next()
+		{
+			if (i >= n) { config.onFinish(path[0]); return }
+			else setTimeout(step, 0)
+		}
+
+		function step()
+		{
+			// Bring helpers to local scope, speedier
+			var beginNode = step.beginNode, endNode = step.endNode, skipSpaces = step.skipSpaces
+			
+			var j = Math.min(i+kStepSize,n)
+			while (i < j)
+			{
+				var c = str[i]
+				if      (c == ' ') {      endNode(1); skipSpaces()
+				
+					// Skip ')', we've already called endNode(1)
+					// Example: 'div.one(a ) div.two' -> {div.one:a, div.two}
+					// endNode(1) gets called for the 2 spaces in ' ) ', so we don't want the ')' to cause another endNode(1), which would total 3 calls.
+					if (str[i] == ')') i++
+				}
+				else if (c == ')') { i++; endNode(1);                }
+				else if (c == '(') { i++;             skipSpaces()
+				
+					if (!beginNode())
+					{
+						i+=2; // Skip ')'
+						endNode(1)
+					}
+				}
+				else beginNode()
+			}
+
+			next()
+		}
+
+		step.beginNode = function() {
+			var selector = readSelector(str, i)
+			var newNode = config.beginNode(selector, path.slice(-1)[0])
+			if (newNode) path.push(newNode)
+			i += selector.length
+			return newNode
+		}
+
+		step.endNode = function(walks) {
+			// Ensure path[0], the root, is not removed
+			walks = Math.min(walks, path.length-1)
+			
+			config.endNode && config.endNode(walks)
+			path.splice(-walks)
+		}
+
+		step.skipSpaces = function() { for (; str[i] == ' '; i++); }
+
+		next()
+	}
+})();
+
+mkup.dom = (function() {
+	var kSelector = /^([a-z].*?)?(#.*?)?(\..*?)?$/
+
 	function make$FromSelector(selector)
 	{
 		var parts = selector.match(kSelector)
@@ -13,63 +89,49 @@ mkup = (function() {
 			if (parts[3]) $el.addClass(parts[3].replace(/\./g, ' '))
 
 			return $el
-
-		} else return $('<junk>')
+		}
+		else return null
 	}
 
-	function readSelector (str, i)
+	function beginNode(selector, $current)
 	{
-		for (var j=i, n=str.length; j < n && !kSeparator.test(str[j]); j++);
-		return str.substring(i,j)
+		var $new = make$FromSelector(selector)
+		if ($new && $current) $new.appendTo($current)
+		return $new
 	}
+	
+	function makeRoot() { return $(document.createDocumentFragment()) }
 
-	return function(str, onFinish)
+	return function(str, onFinish) {
+		mkup(str, {
+			makeRoot: makeRoot,
+			beginNode: beginNode,
+			onFinish: onFinish
+		})
+	}
+})();
+
+mkup.obj = (function() {
+	var kSelector = /^\S+$/
+
+	function beginNode(selector, current)
 	{
-		var $root, $current = $()
-		var i = 0, n = str.length
-
-		function next()
+		if (selector.match(kSelector, current))
 		{
-			if (i >= n) { onFinish($root); return }
-			else setTimeout(step, 0)
-		}
+			var newNode = {}
+			if (current) current[selector] = newNode;
+			return newNode;
 
-		function step()
-		{
-			var c = str[i]
-			if      (c == ' ') {      step.endElement();  step.skipSpaces()   }
-			else if (c == '(') { i++; step.skipSpaces();  step.beginElement() }
-			else if (c == ')') { i++;
-
-				if ($current.is('junk')) { $current.remove() }
-
-				step.endElement(); step.endElement(); step.skipSpaces()
-
-			} else step.beginElement()
-
-			next()
-		}
-
-		step.skipSpaces = function() { for (; str[i] == ' '; i++); }
-
-		step.beginElement = function()
-		{
-			var selector = readSelector(str, i)
-			var $new = make$FromSelector(selector)
-
-			if ($current.length) $new.appendTo($current)
-			else $root = $new
-
-			$current = $new
-			i += selector.length
-		}
-
-		step.endElement = function()
-		{
-			var $parent = $current.parent()
-			if ($parent.length) $current = $parent
-		}
-
-		next()
+		} else return null;
 	}
-})()
+
+	function makeRoot() { return {} }
+	
+	return function(str, onFinish) {
+		mkup(str, {
+			makeRoot: makeRoot,
+			beginNode: beginNode,
+			onFinish: onFinish
+		});
+	};
+})();
